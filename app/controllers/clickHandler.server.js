9a67753c597aa8df1f86b7c25b4a67ae7158efe6
Nick Hu <nick@nickhu.info>
2016-02-23 03:38:28 +0000

    natural time

--- app/controllers/clickHandler.server.js
+++ app/controllers/clickHandler.server.js
@@ -3,12 +3,26 @@
 var Users = require('../models/users.js');
 
 function ClickHandler () {
+	
+	function timeConverter(UNIX_timestamp){
+		  var a = new Date(UNIX_timestamp);
+		  var months = ['January','Febuary','March','April','May','June','July','August','September','October','November','December'];
+		  var year = a.getFullYear();
+		  var month = months[a.getMonth()];
+		  var date = a.getDate();
+		  var time = date + ' ' + month + ' ' + year ;
+		  return time;
+	}
 
 	this.stampIt = function (req, res) {
-		var result;
+		var result = {
+			unix: 	"",
+			natural: ""	
+		};
 		if (req.params.timestamp){
 			var timestamp = Date.parse(req.params.timestamp);
-			result = timestamp;
+			result.unix = timestamp;
+			result.natural = timeConverter(timestamp);
 		}else{
 			result = "error"
 		}
