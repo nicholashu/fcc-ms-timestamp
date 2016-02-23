6e2bfed9d5ab34b29aacbaec432ff3a701218772
Nick Hu <nick@nickhu.info>
2016-02-23 03:30:47 +0000

    unix time

--- app/controllers/clickHandler.server.js
+++ app/controllers/clickHandler.server.js
@@ -4,37 +4,20 @@ var Users = require('../models/users.js');
 
 function ClickHandler () {
 
-	this.getClicks = function (req, res) {
-		Users
-			.findOne({ 'github.id': req.user.github.id }, { '_id': false })
-			.exec(function (err, result) {
-				if (err) { throw err; }
+	this.stampIt = function (req, res) {
+		var result;
+		if (req.params.timestamp){
+			var timestamp = Date.parse(req.params.timestamp);
+			result = timestamp;
+		}else{
+			result = "error"
+		}
+		
+		
 
-				res.json(result.nbrClicks);
-			});
+		res.json(result);
 	};
 
-	this.addClick = function (req, res) {
-		Users
-			.findOneAndUpdate({ 'github.id': req.user.github.id }, { $inc: { 'nbrClicks.clicks': 1 } })
-			.exec(function (err, result) {
-					if (err) { throw err; }
-
-					res.json(result.nbrClicks);
-				}
-			);
-	};
-
-	this.resetClicks = function (req, res) {
-		Users
-			.findOneAndUpdate({ 'github.id': req.user.github.id }, { 'nbrClicks.clicks': 0 })
-			.exec(function (err, result) {
-					if (err) { throw err; }
-
-					res.json(result.nbrClicks);
-				}
-			);
-	};
 
 }
 
