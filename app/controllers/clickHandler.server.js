d0ab6a1e0b23b2134204c2f07acc02178478fc41
Nick Hu <nick@nickhu.info>
2016-02-24 03:34:56 +0000

    functions sorted

--- app/controllers/clickHandler.server.js
+++ app/controllers/clickHandler.server.js
@@ -4,8 +4,19 @@ var Users = require('../models/users.js');
 
 function ClickHandler () {
 	
-	function timeConverter(UNIX_timestamp){
-		  var a = new Date(UNIX_timestamp);
+	function unixToNatural(UNIX_timestamp){
+		  var a = new Date(UNIX_timestamp*100);
+		  console.log(a);
+		  var months = ['January','Febuary','March','April','May','June','July','August','September','October','November','December'];
+		  var year = a.getFullYear();
+		  var month = months[a.getMonth()];
+		  var date = a.getDate();
+		  var time = date + ' ' + month + ' ' + year ;
+		  return time;
+	}
+	
+	function naturalToUnix(NAT_timestamp){
+		  var a = new Date(NAT_timestamp);
 		  var months = ['January','Febuary','March','April','May','June','July','August','September','October','November','December'];
 		  var year = a.getFullYear();
 		  var month = months[a.getMonth()];
@@ -19,10 +30,17 @@ function ClickHandler () {
 			unix: 	"",
 			natural: ""	
 		};
+		var timestamp;
 		if (req.params.timestamp){
-			var timestamp = Date.parse(req.params.timestamp);
-			result.unix = timestamp;
-			result.natural = timeConverter(timestamp);
+			if (isNaN(Number(req.params.timestamp))){
+				timestamp = Date.parse(req.params.timestamp)/100;
+				result.unix = timestamp;
+				result.natural = req.params.timestamp;
+			}else{
+				timestamp = req.params.timestamp;
+				result.unix = timestamp;
+				result.natural = unixToNatural(timestamp);
+			};
 		}else{
 			result = "error"
 		}
